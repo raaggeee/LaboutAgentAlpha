70128ec52d43450bee9af6b43ead7892b0e1c503
raaggeee <raaggee08@gmail.com>
2025-12-23 02:42:48 +0530

    debug

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ user_query = st.chat_input("Say something")
 if user_query:
     with st.spinner("Getting your query sorted..."):
         st.session_state.messages.append({"role":"user", "content":user_query})
-        # response_post = requests.post(f"{BASE_URL}post_request?state_id={state_id}", json=messages)
+        response_post = requests.post(f"{BASE_URL}post_request?state_id={state_id}", json=messages)
         response = requests.get(f"{BASE_URL}generate?user_query={user_query}&state_id={state_id}")
         user_result = response.json()
         st.session_state.messages.append({"role":"assistant", "content":user_result["message"]})
+        print(st.session_state.messages)
         
     st.subheader("  ")
     st.write(user_result["message"])
