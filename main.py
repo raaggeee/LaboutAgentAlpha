d719d6ddf60937b08792da78d9356201510c7dc9
raaggeee <raaggee08@gmail.com>
2025-12-31 15:55:53 +0530

    debug

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if "curr_code" not in st.session_state:
 state_id = st.session_state.uid
 messages = st.session_state.messages
 # print(state_id)
-response_login = requests.post(f"{BASE_URL}login", json={k: st.user(k) for k in ["is_logged_in", "given_name", "email", "email_verified"]})
+response_login = requests.post(f"{BASE_URL}login", json={k: st.user.get(k) for k in ["is_logged_in", "given_name", "email", "email_verified"]})
 
 config = {"configurable": {"thread_id": state_id}}
 
