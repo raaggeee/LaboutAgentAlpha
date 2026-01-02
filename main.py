7ae25ec4dd3dc4eb06288e0de29728cdcb7ac6b0
raaggeee <raaggee08@gmail.com>
2026-01-03 00:32:04 +0530

    added states

--- main.py
+++ main.py
@@ -92,15 +92,27 @@ if st.session_state.limit == 10:
         st.write("Thanks for the conversation.")
         name = st.text_input("First Name")
         phone_no = st.text_input
+
+option_states = st.selectbox(
+    "For State specific answer",
+    ("Center", "Chandigarh", "Delhi", "Haryana", "Rajasthan", "Telangana", "Uttar Pradesh"),
+    placeholder="Select a State..."
+    
+)
         
-option = st.selectbox(
+option_codes = st.selectbox(
             "For Code specific answer",
             ("Introduction to Labour Codes", "The Industry Relation Codes, 2020", "The Codes on Social Security, 2020", "The Codes on Wages, 2019", "The Occupation, Safety, Health and Working Condition Code, 2020"),
             placeholder="Select a Labour Law Code..."
             
-        )
+)
 
-send_settings = requests.post(f"{BASE_URL}send_settings?state_id={state_id}&conversation_code={option}")
+option_json = {
+    "state_id": state_id,
+    "conversation_code": option_codes,
+    "india_state": option_states
+}
+send_settings = requests.post(f"{BASE_URL}send_settings", json=option_json)
 #for logout
 st.button("Log out", on_click=st.logout)
 
