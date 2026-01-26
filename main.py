f4cf3e65992e9d367445e12d9a24e21115b09887
raaggeee <raaggee08@gmail.com>
2026-01-27 00:26:27 +0530

    remove feedback

--- main.py
+++ main.py
@@ -210,18 +210,6 @@ send_settings = requests.post(f"{BASE_URL}send_settings", json=option_json)
 #for logout
 st.button("Log out", on_click=st.logout)
 
-with st.form("main_form"):
-    col1, col2 = st.columns([4, 1])
-
-    with col1:
-        email = st.text_input("Email")
-
-    with col2:
-        st.markdown(" ")
-        feedback = st.text_input("Feedback", label_visibility="collapsed")
-
-    submit = st.form_submit_button("Submit")
-
 post_feedback = requests.post(f"{BASE_URL}send_feedback", json=option_json)
 
 # if user_query:
